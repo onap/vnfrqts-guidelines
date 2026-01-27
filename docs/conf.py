a7c74a931c5185e7e6e042f1cc6fee8938f6f1fa
Matthew Watkins <mwatkins@linuxfoundation.org>
2026-01-27 15:30:09 +0000

    Chore: Modernize docs configuration for Python 3.12+
    Remove broken/outdated Python modules that are incompatible with modern
    Python versions. Replace lfdocs-conf dependency with standalone conf.py.
    Update .readthedocs.yaml to use ubuntu-24.04 and Python 3.13. Modernize
    tox.ini (use allowlist_externals, pin basepython to 3.12, use local
    constraints). Remove obsolete docs/conf.yaml, root tox.ini, and legacy
    templates. Fix ribbon.css max-width per doc-rules. Add local copy of
    etc/upper-constraints.onap.txt from the doc repo.
    
    Removed modules: sphinxcontrib-swaggerdoc, sphinxcontrib-needs,
    sphinxcontrib-nwdiag, sphinx_bootstrap_theme, lfdocs-conf, tox, doc8,
    six, Sphinx==1.8.1.
    
    Issue-ID: CIMAN-33
    Change-Id: Iad6e67a99b34289148d08c84e4a654a462536394
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,43 @@
-from docs_conf.conf import *
+project = "onap"
+release = "master"
+version = "master"
+
+author = "Open Network Automation Platform"
+# yamllint disable-line rule:line-length
+copyright = "ONAP. Licensed under Creative Commons Attribution 4.0 International License"
+
+exclude_patterns = [
+    '.tox'
+]
+
+pygments_style = "sphinx"
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "style_nav_header_background": "white",
+    "sticky_navigation": "False" }
+html_logo = "_static/logo_onap_2017.png"
+html_favicon = "_static/favicon.ico"
+html_static_path = ["_static"]
+html_show_sphinx = False
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.graphviz',
+    'sphinxcontrib.blockdiag',
+    'sphinxcontrib.seqdiag',
+    'sphinxcontrib.plantuml',
+]
 
 branch = 'latest'
 master_doc = 'index'
-
-linkcheck_ignore = [
-    'http://localhost',
-]
 
 intersphinx_mapping = {}
 
 html_last_updated_fmt = '%d-%b-%y %H:%M'
 
-
 def setup(app):
     app.add_css_file("css/ribbon.css")
+
+linkcheck_ignore = [
+    r'http://localhost:\d+/',
+]
